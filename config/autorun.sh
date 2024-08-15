22583132267af4ed0095a658062758732c02b184
uhm0311 <uhm0311@jam2in.com>
2024-08-15 20:09:31 +0900

    TEST: Stop test on failure.

--- config/autorun.sh
+++ config/autorun.sh
@@ -9,3 +9,7 @@ then
 fi
 
 autoreconf --install --force --verbose -Wall
+
+sed 's/"$$tst" $(AM_TESTS_FD_REDIRECT)/"$$tst" $(AM_TESTS_FD_REDIRECT) \&\& \\\n\tTRS="$$(echo $@ | sed s\/.log\/.trs\/g | xargs cat | grep "PASS")" \&\& \\\n\ttest -n "$$TRS" \&\& echo "" || exit 1;/g' Makefile.in > Makefile.out
+rm -f Makefile.in
+yes | mv -f Makefile.out Makefile.in
